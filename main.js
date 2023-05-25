b60aaeed8944d1b8ca4cd75225ea3cde24df1cb8
Rivaldi Alwi <rivaldialwi42@gmail.com>
2023-05-25 14:08:20 +0700

    Push sesi 7

---
+++ main.js
@@ -0,0 +1,7 @@
+//let arr = [0, 1, 2, 3];
+//arr.push(4);
+//console.log(arr);
+
+const num = [5, 5, 5];
+const result = num.reduce((accumulator, currentValue) => accumulator + currentValue, 10);
+console.log(result);
